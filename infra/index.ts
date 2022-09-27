507f35de6b0827321eef568f6bdf68343333ee37
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2022-09-27 15:17:40 +0200

    🧙‍♂

--- infra/index.ts
+++ infra/index.ts
@@ -103,6 +103,8 @@ const securityGroup = new aws.ec2.SecurityGroup(`${pulumi.getStack()}-sg`, {
   }
 })
 
+
+
 const web = new aws.ec2.Instance(`${pulumi.getStack()}-server`, {
   subnetId: publicSubnet.id,
   associatePublicIpAddress: true,
@@ -116,140 +118,11 @@ const web = new aws.ec2.Instance(`${pulumi.getStack()}-server`, {
   userData: fs.readFileSync("server-init.sh", "utf8"),
 });
 
-// const cert = new aws.acm.Certificate("cert", {
-//     domainName: "robosnipers.com",
-//     subjectAlternativeNames: ["robosnipers.com", "www.robosnipers.com"],
-//     tags: {
-//         Name: "drops",
-//     },
-//     validationMethod: "DNS",
-// });
-
-const alb = new aws.lb.LoadBalancer(`${pulumi.getStack()}-alb`, {
-  internal: false,
-  loadBalancerType: "application",
-  securityGroups: [securityGroup.id],
-  subnets: [publicSubnet.id, publicSubnet2.id],
-  enableDeletionProtection: false,
-  tags: {
-    Name: "drops",
-  },
-});
-
-const targetGroup = new aws.lb.TargetGroup(`${pulumi.getStack()}-alb-target`, {
-  port: 80,
-  protocol: "HTTP",
-  vpcId: vpc.id,
-  healthCheck: {
-    healthyThreshold: 2,
-    unhealthyThreshold: 2,
-    timeout: 3,
-    path: "/elb-status",
-    interval: 30,
-  },
-});
-
-const targetGroupAttachment = new aws.lb.TargetGroupAttachment(`${pulumi.getStack()}-target-association`, {
-  targetGroupArn: targetGroup.arn,
-  targetId: web.id,
-  port: 80,
-});
-
-// const secureListener = new aws.lb.Listener("https-listener", {
-//     loadBalancerArn: alb.id,
-//     protocol: "HTTPS",
-//     port: 443,
-//     sslPolicy: "ELBSecurityPolicy-2016-08",
-//     certificateArn: cert.arn,
-//     defaultActions: [
-//         {
-//             type: "forward",
-//             forward: {
-//                 targetGroups: [
-//                     {
-//                         arn: targetGroup.arn
-//                     }
-//                 ],
-//                 stickiness: {
-//                     enabled: true,
-//                     duration: 604800
-//                 }
-//             }
-//         }
-//     ]
-// })
-
-const httpListener = new aws.lb.Listener("http-listener", {
-  loadBalancerArn: alb.id,
-  protocol: "HTTP",
-  port: 80,
-  defaultActions: [
-    // {
-    //     type: "redirect",
-    //     redirect: {
-    //         statusCode: "HTTP_301",
-    //         protocol: "HTTPS",
-    //         port: "443",
-    //         host: "robosnipers.com",
-    //     }
-    // }
-    {
-      type: "forward",
-      forward: {
-        targetGroups: [
-          {
-            arn: targetGroup.arn
-          }
-        ],
-        stickiness: {
-          enabled: true,
-          duration: 604800
-        }
-      }
-    }
-  ]
+const eip = new aws.ec2.Eip(`${pulumi.getStack()}-eip`, {
+  vpc: true,
+  instance: web.id
 })
 
 
-// const primary = new aws.route53.Zone("drs", {
-//     name: "robosnipers.com"
-// });
-//
-// const baseRecord = new aws.route53.Record("base-record", {
-//     zoneId: primary.id,
-//     name: "robosnipers.com",
-//     type: "A",
-//     aliases: [{
-//         name: alb.dnsName,
-//         zoneId: alb.zoneId,
-//         evaluateTargetHealth: true,
-//     }],
-// })
-//
-// const wwwRecord = new aws.route53.Record("www-record", {
-//     zoneId: primary.id,
-//     name: "www",
-//     type: "CNAME",
-//     ttl: 5,
-//     records: [baseRecord.name],
-// })
-
-// const validationRecord1 = new aws.route53.Record("validation-record1", {
-//     zoneId: primary.id,
-//     type: "CNAME",
-//     name: "_a2fe461d49a64c73d886d8b3692bf238.robosnipers.com.",
-//     ttl: 5,
-//     records: [cert.domainValidationOptions[0].resourceRecordName],
-// })
-//
-// const validationRecord2 = new aws.route53.Record("validation-record2", {
-//     zoneId: primary.id,
-//     type: "CNAME",
-//     name: "_a4fda78699bc3c29e630f28b9353ff96.www.robosnipers.com.",
-//     ttl: 5,
-//     records: [cert.domainValidationOptions[1].resourceRecordName],
-// })
-
-
-export const publicDns = alb.dnsName;
+export const elasticIp = eip.publicIp
 export const instanceIp = web.publicIp;
