b7acf2b515b871d735c442765ab16da28d4d2b08
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2022-09-27 14:52:38 +0200

    🧙‍♂

--- infra/index.ts
+++ infra/index.ts
@@ -1,217 +1,238 @@
+import * as awsx from "@pulumi/awsx";
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 import * as fs from "fs";
+import {registerAutoTags} from "./autotag";
 
-const vpc = new aws.ec2.Vpc("vpc", {
-    cidrBlock: "11.0.0.0/16",
+
+registerAutoTags({
+  app: "drops",
+  stack: pulumi.getStack(),
+  deployed_by: "Oscar Chinellato"
 })
 
-const publicSubnet = new aws.ec2.Subnet("publicSubnet", {
-    vpcId: vpc.id,
-    cidrBlock: "11.0.1.0/24",
-    availabilityZone: "eu-west-1a",
-    tags: {
-        Name: "drops",
-    }
+const vpc = awsx.ec2.Vpc.getDefault();
+
+const publicSubnet = new aws.ec2.Subnet(`${pulumi.getStack()}-public-1`, {
+  vpcId: vpc.id,
+  cidrBlock: "172.31.50.0/24",
+  availabilityZone: "eu-west-1a",
+  tags: {
+    Name: "drops",
+  }
 })
 
-const publicSubnet2 = new aws.ec2.Subnet("publicSubnet2", {
-    vpcId: vpc.id,
-    cidrBlock: "11.0.2.0/24",
-    availabilityZone: "eu-west-1b",
-    tags: {
-        Name: "drops",
-    }
+const publicSubnet2 = new aws.ec2.Subnet(`${pulumi.getStack()}-public-2`, {
+  vpcId: vpc.id,
+  cidrBlock: "172.31.51.0/24",
+  availabilityZone: "eu-west-1b",
+  tags: {
+    Name: "drops",
+  }
 })
 
-const gateway = new aws.ec2.InternetGateway("gateway", {
-    vpcId: vpc.id,
-    tags: {
-        Name: "drops",
-    }
+aws.ec2.getInternetGateway({
+  internetGatewayId: "igw-a2b63ec6",
 })
-
-const routeTable = new aws.ec2.RouteTable("routeTable", {
-    vpcId: vpc.id,
-    routes: [
+  .then(gateway => {
+    const routeTable = new aws.ec2.RouteTable(`${pulumi.getStack()}-rt`, {
+      vpcId: vpc.id,
+      routes: [
         {
-            cidrBlock: "0.0.0.0/0",
-            gatewayId: gateway.id,
+          cidrBlock: "0.0.0.0/0",
+          gatewayId: gateway.id,
         }
-    ],
-    tags: {
+      ],
+      tags: {
         Name: "drops",
-    }
-})
+      }
+    })
 
-const association = new aws.ec2.RouteTableAssociation("association", {
-    subnetId: publicSubnet.id,
-    routeTableId: routeTable.id,
-})
+    const association = new aws.ec2.RouteTableAssociation(`${pulumi.getStack()}-ts-association`, {
+      subnetId: publicSubnet.id,
+      routeTableId: routeTable.id,
+    })
 
-const association2 = new aws.ec2.RouteTableAssociation("association2", {
-    subnetId: publicSubnet2.id,
-    routeTableId: routeTable.id,
-})
+    const association2 = new aws.ec2.RouteTableAssociation(`${pulumi.getStack()}-ts-association-2`, {
+      subnetId: publicSubnet2.id,
+      routeTableId: routeTable.id,
+    })
+  })
 
-const securityGroup = new aws.ec2.SecurityGroup("drops-sg", {
-    vpcId: vpc.id,
-    description: "Drops security group",
-    ingress: [
-        {
-            fromPort: 22,
-            protocol: "tcp",
-            toPort: 22,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            fromPort: 80,
-            protocol: "tcp",
-            toPort: 80,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            fromPort: 443,
-            protocol: "tcp",
-            toPort: 443,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            fromPort: 8080,
-            protocol: "tcp",
-            toPort: 8081,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-    ],
-    egress: [
-        {
-            fromPort: 0,
-            protocol: "-1",
-            toPort: 0,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
 
-    ],
-    tags: {
-        Name: "drops",
-    }
-})
-
-const web = new aws.ec2.Instance("drops-server", {
-    subnetId: publicSubnet.id,
-    associatePublicIpAddress: true,
-    vpcSecurityGroupIds: [securityGroup.id],
-    ami: "ami-0d71ea30463e0ff8d",
-    instanceType: "t3.small",
-    tags: {
-        Name: "drops",
+const securityGroup = new aws.ec2.SecurityGroup(`${pulumi.getStack()}-sg`, {
+  vpcId: vpc.id,
+  description: "Drops security group",
+  ingress: [
+    {
+      fromPort: 22,
+      protocol: "tcp",
+      toPort: 22,
+      cidrBlocks: ["0.0.0.0/0"],
     },
-    keyName: "citd",
-    userData: fs.readFileSync("server-init.sh", "utf8"),
-});
-
-const cert = new aws.acm.Certificate("cert", {
-    domainName: "robosnipers.com",
-    subjectAlternativeNames: ["robosnipers.com", "www.robosnipers.com"],
-    tags: {
-        Name: "drops",
+    {
+      fromPort: 80,
+      protocol: "tcp",
+      toPort: 80,
+      cidrBlocks: ["0.0.0.0/0"],
     },
-    validationMethod: "DNS",
-});
-
-const alb = new aws.lb.LoadBalancer("drops-alb", {
-    internal: false,
-    loadBalancerType: "application",
-    securityGroups: [securityGroup.id],
-    subnets: [publicSubnet.id, publicSubnet2.id],
-    enableDeletionProtection: false,
-    tags: {
-        Name: "drops",
+    {
+      fromPort: 443,
+      protocol: "tcp",
+      toPort: 443,
+      cidrBlocks: ["0.0.0.0/0"],
     },
-});
-
-const targetGroup = new aws.lb.TargetGroup("drops-target", {
-    port: 80,
-    protocol: "HTTP",
-    vpcId: vpc.id,
-    healthCheck: {
-        healthyThreshold: 2,
-        unhealthyThreshold: 2,
-        timeout: 3,
-        path: "/elb-status",
-        interval: 30,
+    {
+      fromPort: 8080,
+      protocol: "tcp",
+      toPort: 8081,
+      cidrBlocks: ["0.0.0.0/0"],
     },
-});
+  ],
+  egress: [
+    {
+      fromPort: 0,
+      protocol: "-1",
+      toPort: 0,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
 
-const targetGroupAttachment = new aws.lb.TargetGroupAttachment("drops-target-association", {
-    targetGroupArn: targetGroup.arn,
-    targetId: web.id,
-    port: 80,
-});
-
-const secureListener = new aws.lb.Listener("https-listener", {
-    loadBalancerArn: alb.id,
-    protocol: "HTTPS",
-    port: 443,
-    sslPolicy: "ELBSecurityPolicy-2016-08",
-    certificateArn: cert.arn,
-    defaultActions: [
-        {
-            type: "forward",
-            forward: {
-                targetGroups: [
-                    {
-                        arn: targetGroup.arn
-                    }
-                ],
-                stickiness: {
-                    enabled: true,
-                    duration: 604800
-                }
-            }
-        }
-    ]
+  ],
+  tags: {
+    Name: "drops",
+  }
 })
+
+const web = new aws.ec2.Instance(`${pulumi.getStack()}-server`, {
+  subnetId: publicSubnet.id,
+  associatePublicIpAddress: true,
+  vpcSecurityGroupIds: [securityGroup.id],
+  ami: "ami-0d71ea30463e0ff8d",
+  instanceType: "t3.small",
+  tags: {
+    Name: "drops",
+  },
+  keyName: "citd",
+  userData: fs.readFileSync("server-init.sh", "utf8"),
+});
+
+// const cert = new aws.acm.Certificate("cert", {
+//     domainName: "robosnipers.com",
+//     subjectAlternativeNames: ["robosnipers.com", "www.robosnipers.com"],
+//     tags: {
+//         Name: "drops",
+//     },
+//     validationMethod: "DNS",
+// });
+
+const alb = new aws.lb.LoadBalancer(`${pulumi.getStack()}-alb`, {
+  internal: false,
+  loadBalancerType: "application",
+  securityGroups: [securityGroup.id],
+  subnets: [publicSubnet.id, publicSubnet2.id],
+  enableDeletionProtection: false,
+  tags: {
+    Name: "drops",
+  },
+});
+
+const targetGroup = new aws.lb.TargetGroup(`${pulumi.getStack()}-alb-target`, {
+  port: 80,
+  protocol: "HTTP",
+  vpcId: vpc.id,
+  healthCheck: {
+    healthyThreshold: 2,
+    unhealthyThreshold: 2,
+    timeout: 3,
+    path: "/elb-status",
+    interval: 30,
+  },
+});
+
+const targetGroupAttachment = new aws.lb.TargetGroupAttachment(`${pulumi.getStack()}-target-association`, {
+  targetGroupArn: targetGroup.arn,
+  targetId: web.id,
+  port: 80,
+});
+
+// const secureListener = new aws.lb.Listener("https-listener", {
+//     loadBalancerArn: alb.id,
+//     protocol: "HTTPS",
+//     port: 443,
+//     sslPolicy: "ELBSecurityPolicy-2016-08",
+//     certificateArn: cert.arn,
+//     defaultActions: [
+//         {
+//             type: "forward",
+//             forward: {
+//                 targetGroups: [
+//                     {
+//                         arn: targetGroup.arn
+//                     }
+//                 ],
+//                 stickiness: {
+//                     enabled: true,
+//                     duration: 604800
+//                 }
+//             }
+//         }
+//     ]
+// })
 
 const httpListener = new aws.lb.Listener("http-listener", {
-    loadBalancerArn: alb.id,
-    protocol: "HTTP",
-    port: 80,
-    defaultActions: [
-        {
-            type: "redirect",
-            redirect: {
-                statusCode: "HTTP_301",
-                protocol: "HTTPS",
-                port: "443",
-                host: "robosnipers.com",
-            }
+  loadBalancerArn: alb.id,
+  protocol: "HTTP",
+  port: 80,
+  defaultActions: [
+    // {
+    //     type: "redirect",
+    //     redirect: {
+    //         statusCode: "HTTP_301",
+    //         protocol: "HTTPS",
+    //         port: "443",
+    //         host: "robosnipers.com",
+    //     }
+    // }
+    {
+      type: "forward",
+      forward: {
+        targetGroups: [
+          {
+            arn: targetGroup.arn
+          }
+        ],
+        stickiness: {
+          enabled: true,
+          duration: 604800
         }
-    ]
+      }
+    }
+  ]
 })
 
 
-const primary = new aws.route53.Zone("drs", {
-    name: "robosnipers.com"
-});
-
-const baseRecord = new aws.route53.Record("base-record", {
-    zoneId: primary.id,
-    name: "robosnipers.com",
-    type: "A",
-    aliases: [{
-        name: alb.dnsName,
-        zoneId: alb.zoneId,
-        evaluateTargetHealth: true,
-    }],
-})
-
-const wwwRecord = new aws.route53.Record("www-record", {
-    zoneId: primary.id,
-    name: "www",
-    type: "CNAME",
-    ttl: 5,
-    records: [baseRecord.name],
-})
+// const primary = new aws.route53.Zone("drs", {
+//     name: "robosnipers.com"
+// });
+//
+// const baseRecord = new aws.route53.Record("base-record", {
+//     zoneId: primary.id,
+//     name: "robosnipers.com",
+//     type: "A",
+//     aliases: [{
+//         name: alb.dnsName,
+//         zoneId: alb.zoneId,
+//         evaluateTargetHealth: true,
+//     }],
+// })
+//
+// const wwwRecord = new aws.route53.Record("www-record", {
+//     zoneId: primary.id,
+//     name: "www",
+//     type: "CNAME",
+//     ttl: 5,
+//     records: [baseRecord.name],
+// })
 
 // const validationRecord1 = new aws.route53.Record("validation-record1", {
 //     zoneId: primary.id,
